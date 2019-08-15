890327fcccaa9c0fd445e40273e16a7b098c622b
Pitipon Ratchasupa <pitipon.ra@inet.co.th>
2019-08-15 16:35:28 +0700

    taw

--- admin/customer.php
+++ admin/customer.php
@@ -9,34 +9,6 @@ if(empty($_SESSION["user"])){
 require("../connection.php");
 require("nav.php");
 }
-?>        
-?>
-           
-
-<nav class="navbar navbar-expand-lg navbar-light bg-light cus">
-<a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse taxt" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="./product.php">Product</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="./customer.php">Customer</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          username
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">logout</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
+?>      
     </body>
 </html>
--- admin/nav.php
+++ admin/nav.php
@@ -15,10 +15,10 @@
   <div class="collapse navbar-collapse taxt" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Product</a>
+        <a class="nav-link" href="product.php">Product</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="./customer.php">Customer</a>
+        <a class="nav-link" href="customer.php">Customer</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
