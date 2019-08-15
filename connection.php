335da2824830cc575f126d3772672b471a4f99e8
PloyAriya <ploylovea_22@hotmail.com>
2019-08-15 11:23:22 +0700

    add_connection_db

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+$serverName = "localhost";
+$userName = "root";
+$userPassword = "1234";
+$dbName = "doll_db";
+
+$conn = @mysqli_connect(
+    $serverName,
+    $userName,
+    $userPassword,
+    $dbName
+);
+// if(@mysqli_connect_errno()){
+//     echo "Database Connect Failed : " . @mysqli_connect_error();
+
+// }else{
+//     echo "Database Connected.";
+// }
+@mysqli_close($conn);
+?>
